e5cfdbacb1e43946251227c521029e049653dab4
olehsalo <43565118+olehsalo@users.noreply.github.com>
2018-10-02 11:37:29 +0300

    Create lab2.c

---
+++ lab2.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<math.h>
+
+int main ()
+{
+     //here I am initializing type double variables
+    double a, n, s;
+    //I'm using cycle 'for' to calculate "a" and add it to the sum(s)
+    for (n = 0; n < 7; n++)
+    {
+         a = n*n/pow(M_E, pow(n, 1/2));
+         s +=a;
+         ++n;
+
+    }
+    //here the result is being ptinted
+    printf("%lf", s) ;
+    return 0;
+}
