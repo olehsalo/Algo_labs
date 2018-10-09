9580eddc541ef489d39a64f02ddbdfb4003ed4d8
olehsalo <43565118+olehsalo@users.noreply.github.com>
2018-10-09 10:19:22 +0300

    Update lab2.c

--- lab2.c
+++ lab2.c
@@ -5,7 +5,7 @@ int main ()
 {
      //here I am initializing type double variables
     double a, n, s;
-    //I'm using cycle 'for' to calculate "a" and add it to the sum(s)
+    //I'm using loop 'for' to calculate "a" and add it to the sum(s)
     for (n = 0; n < 7; n++)
     {
          a = n*n/pow(M_E, pow(n, 1/2));
