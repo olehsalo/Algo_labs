47172d51b3aa51dcaa040fbc9f9585e561868db0
olehsalo <43565118+olehsalo@users.noreply.github.com>
2018-10-16 09:08:39 +0300

    Update lab2.c

--- lab2.c
+++ lab2.c
@@ -1,16 +1,16 @@
 #include<stdio.h>
 #include<math.h>
-
+#define M_E 2.71
 int main ()
 {
      //here I am initializing type double variables
-    double a, n, s;
-    //I'm using loop 'for' to calculate "a" and add it to the sum(s)
+    double a, n, s=0;
+    //I'm using cycle 'for' to calculate "a" and add it to the sum(s)
     for (n = 0; n < 7; n++)
     {
          a = n*n/pow(M_E, pow(n, 1/2));
          s +=a;
-         ++n;
+
 
     }
     //here the result is being ptinted
