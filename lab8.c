57fa629ade8069c712e6a94c80b85715e6127741
olehsalo <43565118+olehsalo@users.noreply.github.com>
2018-11-27 19:47:25 +0200

    Update lab8.c

--- lab8.c
+++ lab8.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 struct student
 {
@@ -14,14 +15,56 @@ struct student
 int save(char * filename, struct student *st, int n);
 int load(char * filename);
 
-int main(void)
+int main()
 {
-	char * filename = "students.dat";
-	struct student students[] = { {"Tom", "28ma.04.2001", "fredra street", 99},{ "Alice","15.05.2001","fredra street", 82},{ "Bob","25.09.2001","fredra street", 89}, {"Kate","25.09.2001" ,"fredra street",78} };
-	int n = sizeof(students) / sizeof(students[0]);
-
-	save(filename, students, n);
-	load(filename);
+	char * filename1 = "students1";
+	char * filename2 = "students2";
+	struct student students1[500];
+	struct student students2[500];
+	struct student stud1;
+	int n, i, j, k=0;
+	printf("enter the number of students:");
+   scanf("%d",&n);
+   for(i=0;i<n;i++)
+   {
+      printf( "student %d\nFull", i+1);
+      fgets(stud1.name, 20, stdin);
+      printf( "name :");
+      fgets(stud1.name, 20, stdin);
+      printf("\n");
+      printf( "Birthdate :");
+      fgets(stud1.birthdate, 20, stdin);
+      printf("\n");
+      printf( "Adress :");
+      fgets(stud1.adress, 20, stdin);
+      printf("\n");
+      printf( "Rate :");
+      scanf("%d",&stud1.rate);
+      printf("\n");
+      students1[i]=stud1;
+   }
+   printf("File 1:\n");
+	save(filename1, students1, n);
+   load(filename1);
+   printf("\n");
+   for(i=0;i<n;i++)
+   {
+   	for(j=0;j<n;j++)
+   	{
+   		if(i!=j)
+   		{
+   		   if(strcmp(students1[i].birthdate,students1[j].birthdate)!=0)
+   		   {
+   			   students2[k]=students1[i];
+   			   k++;
+   		   }
+   		}
+   	}
+   }
+   printf("File 2:\n");
+	save(filename2, students2, k);
+   load(filename2);
+   printf("\n");
 	return 0;
 }
 int save(char * filename, struct student * st, int n)
